06db53b9b9a9860d57d3fe0821471dbc7856ac2d
Aleksander Binion <alek.binion@mongodb.com>
2021-04-07 17:35:49 -0400

    cc - remove comments

--- source/usage-examples/findOne.go
+++ source/usage-examples/findOne.go
@@ -14,39 +14,29 @@ func main() {
 
 	// Replace the uri string with your MongoDB deployment's connection string.
 	uri := "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority"
-
-	// Create an empty context
 	ctx := context.TODO()
-
-	// Connect to your MongoDB deployment
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
 	if err != nil {
 		panic(err)
 	}
-	// Disconnect the client once the function returns
+	// Disconnect the client once the function returns.
 	defer func() {
 		if err = client.Disconnect(ctx); err != nil {
 			panic(err)
 		}
 	}()
-	// Define Collection
 	coll := client.Database("sample_mflix").Collection("movies")
 	var result bson.M
-	// Initialize an empty *FindOneoptions value
 	opts := options.FindOne()
-	// Project only the `title` and `imdb` fields in the returned document
 	opts = opts.SetProjection(bson.M{"_id": 0, "title": 1, "imdb": 1})
-	// Sort matched documents in descending order by rating
 	opts = opts.SetSort(bson.D{{"rating", -1}})
-	// Retrieve your document using a query and your configuration options
 	err = coll.FindOne(ctx, bson.M{"title": "The Room"}, opts).Decode(&result)
 	if err != nil {
-		// ErrNoDocuments means that the filter did not match any documents in the collection
 		if err == mongo.ErrNoDocuments {
+			// This error means your filter did not match any documents.
 			return
 		}
 		log.Fatal(err)
 	}
-	// Print your matched document
 	fmt.Printf("%v\n", result)
 }
