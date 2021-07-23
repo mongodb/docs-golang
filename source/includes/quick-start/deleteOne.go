ecceaf726e0336191a84842f55c25071ac9a2d59
Kailie Yuan <kailie.yuan@10gen.com>
2021-07-23 15:21:08 -0400

    added usage example deleteOne page

---
+++ source/includes/quick-start/deleteOne.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri = "mongodb+srv://m220student:m220student@mflix.jojrz.mongodb.net/myFirstDatabase?retryWrites=true&writeConcern=majority"
+	
+func main() {
+
+	ctx := context.TODO()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	// begin deleteOne
+	coll := client.Database("sample_mflix").Collection("movies")
+
+	result, err := coll.DeleteOne(ctx, bson.D{{"title", "The Room"}})
+	// end deleteOne
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Printf("%v document was deleted.\n", result.DeletedCount)
+}
