5851756a3cb4f500e5e622240aa0e6dd5453dfce
Kailie Yuan <kailie.yuan@10gen.com>
2021-07-29 15:52:16 -0400

    updated code snippet

--- source/includes/usage-examples/code-snippets/deleteMany.go
+++ source/includes/usage-examples/code-snippets/deleteMany.go
@@ -27,13 +27,13 @@ func main() {
 
 	// begin deleteMany
 	coll := client.Database("sample_mflix").Collection("movies")
-	filter := bson.D{{"runtime": bson.D{"$gt": 800}}}
+	filter := bson.D{{"runtime": bson.D{{"$gt": 800}}}}
 
 	results, err := coll.DeleteMany(context.TODO(), filter)
 	// end deleteMany
 
 	if err != nil {
-		log.Panic(err)
+		panic(err)
 	}
 
 	// When you run this file for the first time, it should print "Documents deleted: 4"
