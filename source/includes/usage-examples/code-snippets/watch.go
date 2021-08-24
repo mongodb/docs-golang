2504ecb82036e993b8e03a8b20de8399555d2851
Kailie Yuan <kailie.yuan@10gen.com>
2021-08-24 11:38:15 -0400

    added watch usage example page

---
+++ source/includes/usage-examples/code-snippets/watch.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	var uri string
+	if uri = os.Getenv("DRIVER_REF_URI"); uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://docs.mongodb.com/drivers/go/current/usage-examples/")
+	}
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	// begin watch
+	coll := client.Database("insertDB").Collection("haikus")
+	pipeline := mongo.Pipeline{bson.D{{"$match", bson.D{{"operationType", "replace"}}}}}
+    cs, err := coll.Watch(context.TODO(), pipeline, options.ChangeStream().SetFullDocument(options.UpdateLookup))
+    defer cs.Close(context.TODO())
+
+	if err := cs.Err(); err != nil {
+		panic(err)
+	}
+
+	for cs.Next(context.TODO()) {
+		var event bson.D
+		if err := cs.Decode(&event); err != nil {
+			panic(err)
+		}
+		fmt.Println(event)
+	}
+	// end watch
+}
