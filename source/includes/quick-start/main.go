627f2723e2df58aee8ad78f4ffcd82c7812fb0c4
Nathan Leniz <nathan.leniz@mongodb.com>
2021-09-16 11:49:04 -0700

    add godotenv to main.go quickstart

--- source/includes/quick-start/main.go
+++ source/includes/quick-start/main.go
@@ -7,12 +7,17 @@ import (
 	"log"
 	"os"
 
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
 	uri := os.Getenv("MONGODB_URI")
 	if uri == "" {
 		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://docs.mongodb.com/drivers/go/current/usage-examples/")
