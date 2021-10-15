fec59d1c33cf4c0f77086f0964b6dc6825f30cd0
Kailie Yuan <kailie.yuan@10gen.com>
2021-10-15 13:52:16 -0400

    proofread

--- source/includes/fundamentals/code-snippets/CRUD/upsert.go
+++ source/includes/fundamentals/code-snippets/CRUD/upsert.go
@@ -13,7 +13,7 @@ import (
 
 func main() {
 	var uri string
-	if uri = os.Getenv("DRIVER_REF_URI"); uri == "" {
+	if uri = os.Getenv("MONGODB_URI"); uri == "" {
 		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://docs.mongodb.com/drivers/go/current/usage-examples/")
 	}
 
@@ -56,6 +56,7 @@ func main() {
 			panic(err)
 		}
 
+		fmt.Printf("Number of documents updated: %v\n", result.ModifiedCount)
 		fmt.Printf("Number of documents upserted: %v\n", result.UpsertedCount)
 		// end upsert
 	}
