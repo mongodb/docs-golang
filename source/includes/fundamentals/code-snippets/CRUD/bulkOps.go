492ff677be8cfbdaa5087a6e47d7845b35fdb826
Kailie Yuan <kailie.yuan@10gen.com>
2021-10-26 10:40:18 -0400

    restucturing

--- source/includes/fundamentals/code-snippets/CRUD/bulkOps.go
+++ source/includes/fundamentals/code-snippets/CRUD/bulkOps.go
@@ -13,7 +13,7 @@ import (
 
 func main() {
 	var uri string
-	if uri = os.Getenv("DRIVER_REF_URI"); uri == "" {
+	if uri = os.Getenv("MONGODB_URI"); uri == "" {
 		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://docs.mongodb.com/drivers/go/current/usage-examples/")
 	}
 
