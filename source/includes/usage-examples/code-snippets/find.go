43e53ca7a90b2f619c0a841d1cc8a21fb1e120e5
Alison Huh <alison.huh@mongodb.com>
2022-10-25 16:51:47 -0400

    v1.10 backport

--- source/includes/usage-examples/code-snippets/find.go
+++ source/includes/usage-examples/code-snippets/find.go
@@ -9,20 +9,32 @@ import (
 
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+// start-restaurant-struct
+type Restaurant struct {
+	ID           primitive.ObjectID `bson:"_id"`
+	Name         string
+	RestaurantId string `bson:"restaurant_id"`
+	Cuisine      string
+	Address      interface{}
+	Borough      string
+	Grades       interface{}
+}
+
+// end-restaurant-struct
 
 func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found")
 	}
-
 	var uri string
 	if uri = os.Getenv("MONGODB_URI"); uri == "" {
 		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
 	}
-
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
 	if err != nil {
 		panic(err)
@@ -34,8 +46,8 @@ func main() {
 	}()
 
 	// begin find
-	coll := client.Database("sample_training").Collection("zips")
-	filter := bson.D{{"pop", bson.D{{"$lte", 500}}}}
+	coll := client.Database("sample_restaurants").Collection("restaurants")
+	filter := bson.D{{"cuisine", "Italian"}}
 
 	cursor, err := coll.Find(context.TODO(), filter)
 	if err != nil {
@@ -43,11 +55,13 @@ func main() {
 	}
 	// end find
 
-	var results []bson.M
+	var results []Restaurant
 	if err = cursor.All(context.TODO(), &results); err != nil {
 		panic(err)
 	}
+
 	for _, result := range results {
+		cursor.Decode(&result)
 		output, err := json.MarshalIndent(result, "", "    ")
 		if err != nil {
 			panic(err)
