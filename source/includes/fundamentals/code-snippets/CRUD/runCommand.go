cb2e438ec7108f1cf6665f5f13c3fe8d6aaf873c
Rea Radhika Rustagi <rea.rustagi@mongodb.com>
2022-10-27 16:13:06 -0400

    DOCSP-18237: run a command page

---
+++ source/includes/fundamentals/code-snippets/CRUD/runCommand.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+
+	var uri string
+	if uri = os.Getenv("MONGODB_URI"); uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	}
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	// start-runcommand
+	db := client.Database("plants")
+	filter := bson.D{{"info.readOnly", false}}
+	command := bson.D{{"listCollections", 1}, {"filter", filter}}
+
+	cursor, err := db.RunCommandCursor(context.TODO(), command)
+	if err != nil {
+		panic(err)
+	}
+	// end-runcommand
+
+	var results []bson.D
+	if err = cursor.All(context.TODO(), &results); err != nil {
+		panic(err)
+	}
+	for _, result := range results {
+		fmt.Println(result)
+	}
+}
