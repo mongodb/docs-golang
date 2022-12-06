43617488b94bc78383840a460313479722df1ba7
Rea Radhika Rustagi <rea.rustagi@mongodb.com>
2022-12-06 17:07:58 -0500

    DOCSP-26415: structs in agg pg

--- source/includes/fundamentals/code-snippets/aggregation.go
+++ source/includes/fundamentals/code-snippets/aggregation.go
@@ -5,11 +5,22 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+// start-tea-struct
+type Tea struct {
+	Type     string
+	Category string
+	Toppings []string
+	Price    float32
+}
+
+// end-tea-struct
 
 func main() {
 	var uri string
@@ -31,14 +42,14 @@ func main() {
 	// begin insert docs
 	coll := client.Database("tea").Collection("menu")
 	docs := []interface{}{
-		bson.D{{"type", "Masala"}, {"category", "black"}, {"toppings", bson.A{"ginger", "pumpkin spice", "cinnomon"}}, {"price", 6.75}},
-		bson.D{{"type", "Gyokuro"}, {"category", "green"}, {"toppings", bson.A{"berries", "milk foam"}}, {"price", 5.65}},
-		bson.D{{"type", "English Breakfast"}, {"category", "black"}, {"toppings", bson.A{"whipped cream", "honey"}}, {"price", 5.75}},
-		bson.D{{"type", "Sencha"}, {"category", "green"}, {"toppings", bson.A{"lemon", "whipped cream"}}, {"price", 5.15}},
-		bson.D{{"type", "Assam"}, {"category", "black"}, {"toppings", bson.A{"milk foam", "honey", "berries"}}, {"price", 5.65}},
-		bson.D{{"type", "Matcha"}, {"category", "green"}, {"toppings", bson.A{"whipped cream", "honey"}}, {"price", 6.45}},
-		bson.D{{"type", "Earl Grey"}, {"category", "black"}, {"toppings", bson.A{"milk foam", "pumpkin spice"}}, {"price", 6.15}},
-		bson.D{{"type", "Hojicha"}, {"category", "green"}, {"toppings", bson.A{"lemon", "ginger", "milk foam"}}, {"price", 5.55}},
+		Tea{Type: "Masala", Category: "black", Toppings: []string{"ginger", "pumpkin spice", "cinnamon"}, Price: 6.75},
+		Tea{Type: "Gyokuro", Category: "green", Toppings: []string{"berries", "milk foam"}, Price: 5.65},
+		Tea{Type: "English Breakfast", Category: "black", Toppings: []string{"whipped cream", "honey"}, Price: 5.75},
+		Tea{Type: "Sencha", Category: "green", Toppings: []string{"lemon", "whipped cream"}, Price: 5.15},
+		Tea{Type: "Assam", Category: "black", Toppings: []string{"milk foam", "honey", "berries"}, Price: 5.65},
+		Tea{Type: "Matcha", Category: "green", Toppings: []string{"whipped cream", "honey"}, Price: 6.45},
+		Tea{Type: "Earl Grey", Category: "black", Toppings: []string{"milk foam", "pumpkin spice"}, Price: 6.15},
+		Tea{Type: "Hojicha", Category: "green", Toppings: []string{"lemon", "ginger", "milk foam"}, Price: 5.55},
 	}
 
 	result, err := coll.InsertMany(context.TODO(), docs)
@@ -48,7 +59,7 @@ func main() {
 	// end insert docs
 	fmt.Printf("Number of documents inserted: %d\n", len(result.InsertedIDs))
 
-	fmt.Println("Average:")
+	fmt.Println("\nAggregation Example - Average\n")
 	{
 		groupStage := bson.D{
 			{"$group", bson.D{
@@ -71,12 +82,12 @@ func main() {
 			panic(err)
 		}
 		for _, result := range results {
-			fmt.Printf("Average price of %v tea: $%v \n", result["_id"], result["average_price"])
-			fmt.Printf("Amount of %v tea: %v \n\n", result["_id"], result["type_total"])
+			fmt.Printf("Average price of %v tea options: $%v \n", result["_id"], result["average_price"])
+			fmt.Printf("Number of %v tea options: %v \n\n", result["_id"], result["type_total"])
 		}
 	}
 
-	fmt.Println("Unset:")
+	fmt.Println("\nAggregation Example - Unset\n")
 	{
 		matchStage := bson.D{{"$match", bson.D{{"toppings", "milk foam"}}}}
 		unsetStage := bson.D{{"$unset", bson.A{"_id", "category"}}}
@@ -91,12 +102,12 @@ func main() {
 			panic(err)
 		}
 
-		var results []bson.M
+		var results []Tea
 		if err = cursor.All(context.TODO(), &results); err != nil {
 			panic(err)
 		}
 		for _, result := range results {
-			fmt.Printf("Tea: %v \nToppings: %v \nPrice: $%v \n\n", result["type"], result["toppings"], result["price"])
+			fmt.Printf("Tea: %v \nToppings: %v \nPrice: $%v \n\n", result.Type, strings.Join(result.Toppings, ", "), result.Price)
 		}
 	}
 }
