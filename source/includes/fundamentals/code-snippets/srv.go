7e20e7fe4201d5514e320e824086b9b8a254cc1c
Rea Radhika Rustagi <rea.rustagi@mongodb.com>
2023-02-23 09:38:27 -0500

    edits

--- source/includes/fundamentals/code-snippets/srv.go
+++ source/includes/fundamentals/code-snippets/srv.go
@@ -14,7 +14,7 @@ const uri = "<connection string>"
 
 func main() {
 
-	// Use the ServerAPI() method to set the Stable API version to 1
+	// Use the SetServerAPIOptions() method to set the Stable API version to 1
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
 	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
 
