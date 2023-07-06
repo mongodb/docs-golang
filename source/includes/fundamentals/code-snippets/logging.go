c423f39b42aeb4850ad4e3dc54f7301d8342882d
Rea Radhika Rustagi <rea.rustagi@mongodb.com>
2023-07-06 10:28:01 -0400

    tech review MD PV

--- source/includes/fundamentals/code-snippets/logging.go
+++ source/includes/fundamentals/code-snippets/logging.go
@@ -12,8 +12,6 @@ import (
 	"github.com/bombsimon/logrusr/v4"
 	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
-	easy "github.com/t-tomalak/logrus-easy-formatter"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -28,8 +26,8 @@ func main() {
 		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
 	}
 	//standardLogging(uri)
-	//customLogging(uri)
-	thirdPartyLogging(uri)
+	customLogging(uri)
+	//thirdPartyLogging(uri)
 }
 
 func standardLogging(uri string) {
@@ -46,18 +44,22 @@ func standardLogging(uri string) {
 	// end-standard-logger
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
-		log.Fatalf("error connecting to MongoDB: %v", err)
+		panic(err)
 	}
 
 	defer client.Disconnect(context.TODO())
 
 	// start-insert
+	type Item struct {
+		Name string
+	}
+
 	coll := client.Database("testDB").Collection("testColl")
-	_, err = coll.InsertOne(context.TODO(), bson.D{{"item", "grapefruit"}})
+	_, err = coll.InsertOne(context.TODO(), Item{Name: "grapefruit"})
 	// end-insert
 
 	if err != nil {
-		log.Fatalf("InsertOne failed: %v", err)
+		panic(err)
 	}
 }
 
@@ -66,13 +68,14 @@ type CustomLogger struct {
 	io.Writer
 	mu sync.Mutex
 }
+
 // end-customlogger-struct
 
 // start-customlogger-funcs
 func (logger *CustomLogger) Info(level int, msg string, _ ...interface{}) {
 	logger.mu.Lock()
 	defer logger.mu.Unlock()
-	if level == 1 {
+	if options.LogLevel(level+1) == options.LogLevelDebug {
 		fmt.Fprintf(logger, "level: %d DEBUG, message: %s\n", level, msg)
 	} else {
 		fmt.Fprintf(logger, "level: %d INFO, message: %s\n", level, msg)
@@ -84,6 +87,7 @@ func (logger *CustomLogger) Error(err error, msg string, _ ...interface{}) {
 	defer logger.mu.Unlock()
 	fmt.Fprintf(logger, "error: %v, message: %s\n", err, msg)
 }
+
 // end-customlogger-funcs
 
 func customLogging(uri string) {
@@ -101,35 +105,39 @@ func customLogging(uri string) {
 		Client().
 		ApplyURI(uri).
 		SetLoggerOptions(loggerOptions)
-    // end-set-customlogger
+		// end-set-customlogger
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
-		log.Fatalf("error connecting to MongoDB: %v", err)
+		panic(err)
 	}
 
 	defer client.Disconnect(context.TODO())
 
-	coll := client.Database("testDB").Collection("testColl")
-	_, err = coll.InsertOne(context.TODO(), bson.D{{"item", "grapefruit"}})
-	if err != nil {
-		log.Fatalf("InsertOne failed: %v", err)
+	type Item struct {
+		Name string
 	}
-	fmt.Println(buf.String())
+
+	coll := client.Database("testDB").Collection("testColl")
+	_, err = coll.InsertOne(context.TODO(), Item{Name: "grapefruit"})
+	if err != nil {
+		panic(err)
+	}
+	log.Println(buf.String())
 }
 
 func thirdPartyLogging(uri string) {
-    // start-make-logrus
+	// start-make-logrus
 	myLogger := &logrus.Logger{
 		Out:   os.Stderr,
 		Level: logrus.DebugLevel,
-		Formatter: &easy.Formatter{
+		Formatter: &logrus.JSONFormatter{
 			TimestampFormat: "2006-01-02 15:04:05",
-			LogFormat:       "[%lvl%]: %time% - %msg% <%commandName%>\n",
+			PrettyPrint:     true,
 		},
 	}
-    // end-make-logrus
+	// end-make-logrus
 
-    // start-set-thirdparty-logger
+	// start-set-thirdparty-logger
 	sink := logrusr.New(myLogger).GetSink()
 
 	loggerOptions := options.
@@ -141,30 +149,212 @@ func thirdPartyLogging(uri string) {
 		Client().
 		ApplyURI(uri).
 		SetLoggerOptions(loggerOptions)
-    // end-set-thirdparty-logger
+		// end-set-thirdparty-logger
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
-		log.Fatalf("error connecting to MongoDB: %v", err)
+		panic(err)
 	}
 
 	defer client.Disconnect(context.TODO())
 
 	// start-log-operations
+	type Item struct {
+		Name string
+	}
+
 	coll := client.Database("testDB").Collection("testColl")
 	docs := []interface{}{
-		bson.D{{"item", "starfruit"}},
-		bson.D{{"item", "kiwi"}},
-		bson.D{{"item", "cantaloupe"}},
+		Item{Name: "starfruit"},
+		Item{Name: "kiwi"},
+		Item{Name: "cantaloupe"},
 	}
 	_, err = coll.InsertMany(context.TODO(), docs)
-	_, err = coll.DeleteOne(context.TODO(), bson.D{{"item", "kiwi"}})
-	_, err = coll.UpdateOne(
-		context.TODO(),
-		bson.D{{"item", "cantaloupe"}},
-		bson.D{{"$set", bson.D{{"qty", 3}}}},
-	)
-	// end-log-operations
 	if err != nil {
-		log.Fatalf("Operation failed: %v", err)
+		panic(err)
 	}
+	_, err = coll.DeleteOne(context.TODO(), Item{Name: "kiwi"})
+	if err != nil {
+		panic(err)
+	}
+	// end-log-operations
+}
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"sync"
+
+	"github.com/bombsimon/logrusr/v4"
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	var uri string
+	if uri = os.Getenv("MONGODB_URI"); uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	}
+	//standardLogging(uri)
+	customLogging(uri)
+	//thirdPartyLogging(uri)
+}
+
+func standardLogging(uri string) {
+	// start-standard-logger
+	loggerOptions := options.
+		Logger().
+		SetMaxDocumentLength(25).
+		SetComponentLevel(options.LogComponentCommand, options.LogLevelDebug)
+
+	clientOptions := options.
+		Client().
+		ApplyURI(uri).
+		SetLoggerOptions(loggerOptions)
+	// end-standard-logger
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		panic(err)
+	}
+
+	defer client.Disconnect(context.TODO())
+
+	// start-insert
+	type Item struct {
+		Name string
+	}
+
+	coll := client.Database("testDB").Collection("testColl")
+	_, err = coll.InsertOne(context.TODO(), Item{Name: "grapefruit"})
+	// end-insert
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+// start-customlogger-struct
+type CustomLogger struct {
+	io.Writer
+	mu sync.Mutex
+}
+
+// end-customlogger-struct
+
+// start-customlogger-funcs
+func (logger *CustomLogger) Info(level int, msg string, _ ...interface{}) {
+	logger.mu.Lock()
+	defer logger.mu.Unlock()
+	if options.LogLevel(level+1) == options.LogLevelDebug {
+		fmt.Fprintf(logger, "level: %d DEBUG, message: %s\n", level, msg)
+	} else {
+		fmt.Fprintf(logger, "level: %d INFO, message: %s\n", level, msg)
+	}
+}
+
+func (logger *CustomLogger) Error(err error, msg string, _ ...interface{}) {
+	logger.mu.Lock()
+	defer logger.mu.Unlock()
+	fmt.Fprintf(logger, "error: %v, message: %s\n", err, msg)
+}
+
+// end-customlogger-funcs
+
+func customLogging(uri string) {
+	// start-set-customlogger
+	buf := bytes.NewBuffer(nil)
+	sink := &CustomLogger{Writer: buf}
+
+	loggerOptions := options.
+		Logger().
+		SetSink(sink).
+		SetComponentLevel(options.LogComponentCommand, options.LogLevelDebug).
+		SetComponentLevel(options.LogComponentConnection, options.LogLevelDebug)
+
+	clientOptions := options.
+		Client().
+		ApplyURI(uri).
+		SetLoggerOptions(loggerOptions)
+		// end-set-customlogger
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		panic(err)
+	}
+
+	defer client.Disconnect(context.TODO())
+
+	type Item struct {
+		Name string
+	}
+
+	coll := client.Database("testDB").Collection("testColl")
+	_, err = coll.InsertOne(context.TODO(), Item{Name: "grapefruit"})
+	if err != nil {
+		panic(err)
+	}
+	log.Println(buf.String())
+}
+
+func thirdPartyLogging(uri string) {
+	// start-make-logrus
+	myLogger := &logrus.Logger{
+		Out:   os.Stderr,
+		Level: logrus.DebugLevel,
+		Formatter: &logrus.JSONFormatter{
+			TimestampFormat: "2006-01-02 15:04:05",
+			PrettyPrint:     true,
+		},
+	}
+	// end-make-logrus
+
+	// start-set-thirdparty-logger
+	sink := logrusr.New(myLogger).GetSink()
+
+	loggerOptions := options.
+		Logger().
+		SetSink(sink).
+		SetComponentLevel(options.LogComponentCommand, options.LogLevelDebug)
+
+	clientOptions := options.
+		Client().
+		ApplyURI(uri).
+		SetLoggerOptions(loggerOptions)
+		// end-set-thirdparty-logger
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		panic(err)
+	}
+
+	defer client.Disconnect(context.TODO())
+
+	// start-log-operations
+	type Item struct {
+		Name string
+	}
+
+	coll := client.Database("testDB").Collection("testColl")
+	docs := []interface{}{
+		Item{Name: "starfruit"},
+		Item{Name: "kiwi"},
+		Item{Name: "cantaloupe"},
+	}
+	_, err = coll.InsertMany(context.TODO(), docs)
+	if err != nil {
+		panic(err)
+	}
+	_, err = coll.DeleteOne(context.TODO(), Item{Name: "kiwi"})
+	if err != nil {
+		panic(err)
+	}
+	// end-log-operations
 }
