da324ed86317d392ed81ecf17a4483369f2e8c36
Lindsey Moore <lindsey.moore@mongodb.com>
2025-03-04 19:40:18 -0500

    comment edit

--- source/includes/fundamentals/code-snippets/indexes/atlasVectorSearch.go
+++ source/includes/fundamentals/code-snippets/indexes/atlasVectorSearch.go
@@ -57,7 +57,7 @@ func main() {
 		Options: opts,
 	}
 
-	// Create the index
+	// Creates the index
 	searchIndexName, err := coll.SearchIndexes().CreateOne(ctx, vectorSearchIndexModel)
 	// end-create-vector-search
 
