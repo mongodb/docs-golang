39c32459086d64ac9b37aef0c12fe57e7ac2e602
Stephanie Aurelio <stephanie.aurelio@mongodb.com>
2025-06-05 16:27:12 -0700

    pv feedback

--- source/includes/connect/connection-pools-uri.go
+++ source/includes/connect/connection-pools-uri.go
@@ -8,18 +8,23 @@ import (
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
-func main() {
-	//start-connection-pool-uri
-	// Connection string with connection pool options
-	uri := "mongodb://localhost:27017/?maxPoolSize=50&minPoolSize=10&maxIdleTimeMS=30000"
+// start-uri-variable
+// Connection string with connection pool options
+const (
+	uri = "mongodb://localhost:27017/?maxPoolSize=50&minPoolSize=10&maxIdleTimeMS=30000"
+)
 
+// end-uri-variable
+func main() {
+	// start-apply-uri
 	// Creates a new client and connect to the server
 	client, err := mongo.Connect(options.Client().ApplyURI(uri))
 	if err != nil {
 		log.Fatal(err)
 	}
-	//end-connection-pool-uri
+	// end-apply-uri
 
+	fmt.log("Connected to MongoDB with connection pool options")
 	defer func() {
 		if err = client.Disconnect(context.TODO()); err != nil {
 			log.Fatal(err)
