ec1c6b1faa5d364261c9cadab8635a9b36ebda8e
Stephanie Aurelio <stephanie.aurelio@mongodb.com>
2025-07-16 16:21:13 -0700

    add update one example

--- source/includes/usage-examples/code-snippets/updateOneBson.go
+++ source/includes/usage-examples/code-snippets/updateOneBson.go
@@ -13,23 +13,6 @@ import (
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
-// Defines a Restaurant struct as a model for documents in the "restaurants" collection
-type Restaurant struct {
-	ID            bson.ObjectID `bson:"_id"`
-	Name          string        `bson:"name"`
-	AverageRating float64       `bson:"avg_rating,omitempty"`
-}
-
-// Create a filter struct to specify the document to update
-type UpdateRestaurantFilter struct {
-	ID bson.ObjectID `bson:"_id"`
-}
-
-// Defines a RestaurantUpdate struct to specify the fields to update
-type RestaurantUpdate struct {
-	AverageRating float64 `bson:"avg_rating"`
-}
-
 func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found")
@@ -51,12 +34,11 @@ func main() {
 	}()
 
 	coll := client.Database("sample_restaurants").Collection("restaurants")
-
-	id, _ := bson.ObjectIDFromHex("5eb3d668b31de5d588f4292b")
-	filter := UpdateRestaurantFilter{ID: id}
+	id, _ := bson.ObjectIDFromHex("5eb3d668b31de5d588f42a7a")
+	filter := bson.D{{"_id", id}}
 
 	// Creates instructions to add the "avg_rating" field to documents
-	update := bson.D{{"$set", Restaurant{AverageRating: 4.4}}}
+	update := bson.D{{"$set", bson.D{{"avg_rating", 4.4}}}}
 
 	// Updates the first document that has the specified "_id" value
 	result, err := coll.UpdateOne(context.TODO(), filter, update)
